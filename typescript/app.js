cfb230642e8da983debf57ddb9ff38d050c64739
The Great White Rabbit <gribomen@mail.ru>
2026-04-01 22:14:08 +0300

    Посмотрел использование типов в TS

--- typescript/app.js
+++ typescript/app.js
@@ -1,4 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const a = 1;
+let revenue = 1000;
+let bonus = 500;
+let res = revenue + bonus;
+console.log(res);
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,1 +1,7 @@
-const a = 1;
+let revenue = 1000;
+let bonus: string = '500';
+let c: string = 'sdf';
+let d: boolean = true;
+
+let res: number = revenue + Number.parseInt(bonus);
+console.log(res);
