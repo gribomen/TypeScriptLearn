adc68804b40eedb22e1a1eb317138dc61a34456f
The Great White Rabbit <gribomen@mail.ru>
2026-04-02 00:10:17 +0300

    Рассмотрел команду readonly нужная для обозначения только для чтения

--- typescript/app.js
+++ typescript/app.js
@@ -1,12 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const skill = [1, 'Dev'];
-const [id, skillName] = skill;
-//const id = skill[0];
-//const skillName = skill[1];
-//skill.push('sdfsf');
-//const  sdf = skill[2];
-//skill.pop()
-const arr = [1, 'sdf', true, true, false];
-console.log(arr);
+const skills = ['Dev', 'DevOps'];
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,12 +1,5 @@
-const skill: [number, string] = [1, 'Dev'];
-const [id, skillName] = skill;
+const skill: readonly [number, string] = [1, 'Dev'];
 
-//const id = skill[0];
-//const skillName = skill[1];
+const skills: ReadonlyArray<string> = ['Dev', 'DevOps'];
 
-//skill.push('sdfsf');
-//const  sdf = skill[2];
-//skill.pop()
-
-const arr: [number, string, ...boolean[]] = [1, 'sdf', true, true, false];
-console.log(arr);
+skill.push();
