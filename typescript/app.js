9421cd414e3d96b457ce6898714d0c07c17fde18
The Great White Rabbit <gribomen@mail.ru>
2026-04-02 10:52:12 +0300

    Учился переопределять типы в функции

--- typescript/app.js
+++ typescript/app.js
@@ -1,14 +1,28 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let info = {
-    "officeId": 45,
-    "isOpened": false,
-    "contacts": {
-        "phone": "+79100000000",
-        "email": "my@email.ru",
-        "address": {
-            "city": "Москва"
-        }
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+var statusReq;
+(function (statusReq) {
+    statusReq["PUBLISHED"] = "published";
+    statusReq["DRAFT"] = "draft";
+    statusReq["DELETED"] = "deleted";
+})(statusReq || (statusReq = {}));
+function getFaqs(req) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield fetch('/faqs', {
+            method: 'POST',
+            body: JSON.stringify(req)
+        });
+        const data = yield res.json();
+        return data;
+    });
+}
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,21 +1,23 @@
-let info: {
-    officeId: number;
-    isOpened: boolean;
-    contacts: {
-        phone: string;
-        email: string;
-        address: {
-            city: string;
-        };
-    };
-} = {
-    "officeId": 45,
-    "isOpened": false,
-    "contacts": {
-        "phone": "+79100000000",
-        "email": "my@email.ru",
-        "address": {
-            "city": "Москва"
-        }
-    }
+enum statusReq {
+    PUBLISHED = 'published',
+    DRAFT = 'draft',
+    DELETED = 'deleted'
+}
+
+async function getFaqs(req: {
+    topicId: number;
+    status?: statusReq;
+}): Promise<{
+    question: string;
+    answer: string;
+    tags: string[];
+    likes: number;
+    status: statusReq;
+}[]> {
+    const res = await fetch('/faqs', {
+        method: 'POST',
+        body: JSON.stringify(req)
+    });
+    const data = await res.json();
+    return data;
 }
