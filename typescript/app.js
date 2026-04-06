ea2c00210e4bb57f81311657e08c5846be49e2a1
The Great White Rabbit <gribomen@mail.ru>
2026-04-06 23:35:34 +0300

    Изучил Get и Set в TS

--- typescript/app.js
+++ typescript/app.js
@@ -1,31 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var PaymentStatus;
-(function (PaymentStatus) {
-    PaymentStatus[PaymentStatus["Holded"] = 0] = "Holded";
-    PaymentStatus[PaymentStatus["Processed"] = 1] = "Processed";
-    PaymentStatus[PaymentStatus["Reversed"] = 2] = "Reversed";
-})(PaymentStatus || (PaymentStatus = {}));
-class Payment {
-    constructor(id) {
-        this.status = PaymentStatus.Holded;
+class User {
+    set login(login) {
+        this._login = 'user-' + login;
         this.createdAt = new Date();
-        this.id = id;
     }
-    getPaymentLifeTime() {
-        return new Date().getTime() - this.createdAt.getTime();
+    get login() {
+        return this._login;
     }
-    unHoldPayment() {
-        if (this.status == PaymentStatus.Processed) {
-            throw new Error('Платеж не может быть возвращен!');
-        }
-        this.status = PaymentStatus.Reversed;
-        this.updatedAt = new Date();
+    getPassword(p) {
+        return __awaiter(this, void 0, void 0, function* () {
+        });
     }
 }
-const payment = new Payment(1);
-payment.unHoldPayment();
-console.log(payment);
-const time = payment.getPaymentLifeTime();
-console.log(time);
+const user = new User();
+user.login = 'myLogin';
+console.log(user);
+console.log(user.login);
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,38 +1,29 @@
-enum PaymentStatus {
-    Holded,
-    Processed,
-    Reversed
+class User {
+    _login: string;
+    password: string;
+    createdAt: Date;
+
+    set login(login: string | number) {
+        this._login = 'user-' + login;
+        this.createdAt = new Date();
+    }
+
+    get login() {
+        return this._login;
+    }
+
+    async getPassword(p: string) {
+
+    }
+
+    // set password(p:string){
+    //  // sync    
+    //}
 }
 
-class Payment {
-    id: number
-    status: PaymentStatus = PaymentStatus.Holded;
-    createdAt: Date = new Date();
-    updatedAt: Date;
+const user = new User();
+user.login = 'myLogin';
+console.log(user);
+console.log(user.login);
 
-    constructor(id: number) {
-        this.id = id;
 
-    }
-
-    getPaymentLifeTime(): number {
-        return new Date().getTime() - this.createdAt.getTime();
-    }
-
-    unHoldPayment(): void {
-        if (this.status == PaymentStatus.Processed) {
-            throw new Error('Платеж не может быть возвращен!');
-
-        }
-
-        this.status = PaymentStatus.Reversed;
-        this.updatedAt = new Date();
-
-    }
-}
-
-const payment = new Payment(1);
-payment.unHoldPayment();
-console.log(payment);
-const time = payment.getPaymentLifeTime();
-console.log(time);
