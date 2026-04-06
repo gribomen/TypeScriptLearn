4404228e591ef71210e10289bf37071a3a0fd252
The Great White Rabbit <gribomen@mail.ru>
2026-04-06 17:33:12 +0300

    Сделал домашку по типам

--- typescript/app.js
+++ typescript/app.js
@@ -1,12 +1,36 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const a = {};
-assertUser(a);
-a.name = 'Вася';
-function assertUser(obj) {
-    if (typeof obj === 'object' && !!obj && 'name' in obj) {
+class CustomError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.code = code;
+        this.name = "CustomError";
+    }
+}
+const url = "https://dummyjson.com/us";
+function isSuccess(response) {
+    if (response.ok == true) {
         return;
     }
-    throw new Error('Не пользователь');
+    else if (response.status == 404) {
+        throw new CustomError("Запрос не выполнен, страница не обнаружена", 404);
+    }
+    throw new Error("Неизвестная ошибка");
 }
+async function getResponse(url) {
+    const response = await fetch(url);
+    try {
+        isSuccess(response);
+        const result = await response.json();
+        console.log(result);
+    }
+    catch (error) {
+        if (error instanceof CustomError) {
+            console.log(`Код ошибки ${error.code}, сообщение: ${error.message}`);
+        }
+        if (error instanceof Error && !(error instanceof CustomError)) {
+            console.log(`Error: ${error.message}`);
+        }
+    }
+}
+getResponse(url);
+export {};
 //# sourceMappingURL=app.js.map
