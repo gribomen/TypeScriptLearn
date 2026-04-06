b68e179854abba238382070fe2c96d630c84a675
The Great White Rabbit <gribomen@mail.ru>
2026-04-07 00:10:56 +0300

    Изучил особенности наследования классов

--- typescript/app.js
+++ typescript/app.js
@@ -25,4 +25,25 @@ class ParsistedPayment extends Payment {
     }
 }
 new ParsistedPayment();
+class User {
+    constructor() {
+        this.name = 'user';
+        console.log(this.name);
+    }
+}
+class Admin extends User {
+    constructor() {
+        super();
+        this.name = 'admin';
+        console.log(this.name);
+    }
+}
+new Admin();
+// new Error('');
+class HttpError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.code = code !== null && code !== void 0 ? code : 500;
+    }
+}
 //# sourceMappingURL=app.js.map
