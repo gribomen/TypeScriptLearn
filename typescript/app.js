9eb4a523601678a64ada1a46fd70c81c509f99df
The Great White Rabbit <gribomen@mail.ru>
2026-04-07 00:00:36 +0300

    Посмотрел на наследование.

--- typescript/app.js
+++ typescript/app.js
@@ -1,28 +1,28 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-class Logger {
-    log(...args) {
-        console.log(...args);
+class Payment {
+    constructor(id) {
+        this.status = 'new';
+        this.id = id;
     }
-    error(...args) {
-        return __awaiter(this, void 0, void 0, function* () {
-            //Кинуть во внешнюю систему
-            console.log(...args);
-        });
+    pay() {
+        this.status = 'paid';
     }
 }
-class User {
-    pay(paymentId) {
-        ///
+class ParsistedPayment extends Payment {
+    constructor() {
+        const id = Math.random();
+        super(id);
+    }
+    save() {
+        // Сохраняет базу
+    }
+    pay(date) {
+        //super.pay();
+        if (date) {
+            this.paidAt = date;
+        }
     }
 }
+new ParsistedPayment();
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,29 +1,38 @@
-interface ILogger {
-    log(...args): void;
-    error(...args): void;
-}
+type PaymentStatus = 'new' | 'paid';
 
-class Logger implements ILogger {
+class Payment {
+    id: number;
+    status: PaymentStatus = 'new';
 
-    log(...args: any[]): void {
-        console.log(...args);
+    constructor(id: number) {
+        this.id = id
     }
 
-    async error(...args: any[]): Promise<void> {
-        //Кинуть во внешнюю систему
-        console.log(...args);
+    pay() {
+        this.status = 'paid';
+    }
+}
+
+class ParsistedPayment extends Payment {
+    databaseId: number;
+    paidAt: Date;
+
+    constructor() {
+        const id = Math.random();
+        super(id);
     }
 
-}
-
-interface IPayable {
-    pay(paymentId: number): void;
-    price?: number;
-}
-
-class User implements IPayable {
-    pay(paymentId: number): void {
-        ///
+    save() {
+        // Сохраняет базу
     }
 
+    override pay(date?: Date) {
+        //super.pay();
+        if (date) {
+            this.paidAt = date;
+        }
+
+    }
 }
+
+new ParsistedPayment()
