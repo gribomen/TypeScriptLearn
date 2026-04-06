404da6cbbdad4286f066d50fd2e9110fdad355d8
The Great White Rabbit <gribomen@mail.ru>
2026-04-06 22:57:59 +0300

    Изучил методы в классах TS

--- typescript/app.js
+++ typescript/app.js
@@ -1,19 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class User {
-    constructor(ageOrName, age) {
-        if (typeof ageOrName === 'string') {
-            this.name = ageOrName;
+var PaymentStatus;
+(function (PaymentStatus) {
+    PaymentStatus[PaymentStatus["Holded"] = 0] = "Holded";
+    PaymentStatus[PaymentStatus["Processed"] = 1] = "Processed";
+    PaymentStatus[PaymentStatus["Reversed"] = 2] = "Reversed";
+})(PaymentStatus || (PaymentStatus = {}));
+class Payment {
+    constructor(id) {
+        this.status = PaymentStatus.Holded;
+        this.createdAt = new Date();
+        this.id = id;
+    }
+    getPaymentLifeTime() {
+        return new Date().getTime() - this.createdAt.getTime();
+    }
+    unHoldPayment() {
+        if (this.status == PaymentStatus.Processed) {
+            throw new Error('Платеж не может быть возвращен!');
         }
-        else if (typeof ageOrName === 'number') {
-            this.age = ageOrName;
-        }
-        if (typeof age === 'number') {
-            this.age = age;
-        }
+        this.status = PaymentStatus.Reversed;
+        this.updatedAt = new Date();
     }
 }
-const user = new User('Вася');
-const user2 = new User();
-const user3 = new User(33);
+const payment = new Payment(1);
+payment.unHoldPayment();
+console.log(payment);
+const time = payment.getPaymentLifeTime();
+console.log(time);
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,24 +1,38 @@
-class User {
-    name: string;
-    age: number;
-
-    constructor();
-    constructor(name: string);
-    constructor(age: number);
-    constructor(name: string, age: number);
-    constructor(ageOrName?: string | number, age?: number) {
-        if (typeof ageOrName === 'string') {
-            this.name = ageOrName;
-        } else if (typeof ageOrName === 'number') {
-            this.age = ageOrName;
-        }
-        if (typeof age === 'number') {
-            this.age = age;
-        }
-    }
-
+enum PaymentStatus {
+    Holded,
+    Processed,
+    Reversed
 }
 
-const user = new User('Вася');
-const user2 = new User();
-const user3 = new User(33)
+class Payment {
+    id: number
+    status: PaymentStatus = PaymentStatus.Holded;
+    createdAt: Date = new Date();
+    updatedAt: Date;
+
+    constructor(id: number) {
+        this.id = id;
+
+    }
+
+    getPaymentLifeTime(): number {
+        return new Date().getTime() - this.createdAt.getTime();
+    }
+
+    unHoldPayment(): void {
+        if (this.status == PaymentStatus.Processed) {
+            throw new Error('Платеж не может быть возвращен!');
+
+        }
+
+        this.status = PaymentStatus.Reversed;
+        this.updatedAt = new Date();
+
+    }
+}
+
+const payment = new Payment(1);
+payment.unHoldPayment();
+console.log(payment);
+const time = payment.getPaymentLifeTime();
+console.log(time);
