5d8dc10d9757f73f269e644ac85c6224c2f09bd3
The Great White Rabbit <gribomen@mail.ru>
2026-04-06 22:40:04 +0300

    Изучил метод работы с конструктором

--- typescript/app.js
+++ typescript/app.js
@@ -1,16 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class User {
-    constructor(name) {
-        this.name = name;
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === 'string') {
+            this.name = ageOrName;
+        }
+        else if (typeof ageOrName === 'number') {
+            this.age = ageOrName;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
 const user = new User('Вася');
-console.log(user);
-user.name = 'Петя';
-console.log(user);
-class Admin {
-}
-const admin = new Admin();
-admin.role = 1;
+const user2 = new User();
+const user3 = new User(33);
 //# sourceMappingURL=app.js.map
