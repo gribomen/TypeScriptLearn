cf43a38895bc9d9276202fc1b585400bf948bb93
The Great White Rabbit <gribomen@mail.ru>
2026-04-07 23:30:40 +0300

    Изучил типизацию this

--- typescript/app.js
+++ typescript/app.js
@@ -1,29 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Payment {
-    constructor() {
-        this.date = new Date();
-        this.getDateArrow = () => {
-            return this.date;
-        };
+class UserBuilder {
+    setName(name) {
+        this.name = name;
+        return this;
     }
-    getDate() {
-        return this.date;
+    isAdmin() {
+        return this instanceof AdminBuilder;
     }
 }
-const p = new Payment();
-const user = {
-    id: 1,
-    paymentDate: p.getDate.bind(p),
-    getDateArrow: p.getDateArrow
-};
-console.log(p.getDate());
-console.log(user.paymentDate());
-console.log(user.getDateArrow());
-class PaymentPersistent extends Payment {
-    save() {
-        return this.getDateArrow();
-    }
+class AdminBuilder extends UserBuilder {
 }
-console.log(new PaymentPersistent().save());
+const res = new UserBuilder().setName('Вася');
+const res2 = new AdminBuilder().setName('Вася');
+let user = new UserBuilder();
+if (user.isAdmin()) {
+    console.log(user);
+}
+else {
+    console.log(user);
+}
 //# sourceMappingURL=app.js.map
