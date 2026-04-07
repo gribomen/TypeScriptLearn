31371bdec85cc2e9e1a4d0b8e8553975a8428c4c
The Great White Rabbit <gribomen@mail.ru>
2026-04-07 23:54:26 +0300

    Сделал абстрактный logger

--- typescript/app.js
+++ typescript/app.js
@@ -1,61 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Delivery {
-    constructor(date) {
-        this.date = date;
+class Logger {
+    printDate(date) {
+        this.log(date.toString());
     }
 }
-class HomeDelivery extends Delivery {
-    constructor(adress, date) {
-        super(date);
-        this.adress = adress;
+class RealLogger extends Logger {
+    log(message) {
+        console.log(message);
+    }
+    logWithDate(date, message) {
+        this.printDate(date);
+        this.log(message);
     }
 }
-class ShopDelivery extends Delivery {
-    constructor(id) {
-        super(new Date());
-        this.id = id;
-    }
-}
-class Cart {
-    constructor() {
-        this.productList = [];
-    }
-    addProduct(product) {
-        this.productList.push(product);
-    }
-    deleteProduct(id) {
-        this.productList = this.productList.filter((p) => p.id !== id);
-    }
-    calculateCostItems() {
-        return this.productList.reduce((sum, product) => sum + product.price, 0);
-    }
-    setDelivery(delivery) {
-        this.delivery = delivery;
-    }
-    checkout() {
-        return (this.delivery
-            && this.productList.length > 0) ? "Ok" : "";
-    }
-}
-const cart = new Cart();
-cart.addProduct({
-    name: "1",
-    price: 1,
-    id: 1
-});
-cart.addProduct({
-    name: "2",
-    price: 2,
-    id: 2
-});
-cart.addProduct({
-    name: "3",
-    price: 3,
-    id: 3
-});
-cart.deleteProduct(1);
-cart.calculateCostItems();
-cart.setDelivery(new HomeDelivery("sss", new Date("2025/05/03")));
-console.log(cart.checkout());
+const r = new RealLogger();
+r.logWithDate(new Date(), "сообщение века");
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,78 +1,20 @@
-type Product = {
-    id: number;
-    name: string;
-    price: number;
-}
-
-class Delivery {
-    date: Date;
-    constructor(date: Date) {
-        this.date = date;
+abstract class Logger {
+    abstract log(message: string): void;
+    printDate(date: Date) {
+        this.log(date.toString())
     }
 }
 
-class HomeDelivery extends Delivery {
-    constructor(public adress: string, date: Date) {
-        super(date);
+class RealLogger extends Logger {
+    log(message: string): void {
+        console.log(message);
+    }
+
+    logWithDate(date: Date, message: string): void {
+        this.printDate(date);
+        this.log(message);
     }
 }
 
-class ShopDelivery extends Delivery {
-    constructor(public id: number) {
-        super(new Date());
-    }
-}
-
-type DeliveryOptions = HomeDelivery | ShopDelivery
-class Cart {
-    private productList: Product[] = [];
-    private delivery: DeliveryOptions;
-
-    public addProduct(product: Product): void {
-        this.productList.push(product);
-
-    }
-
-    public deleteProduct(id: number): void {
-        this.productList = this.productList.filter((p: Product) => p.id !== id);
-    }
-
-    public calculateCostItems(): number {
-        return this.productList.reduce((sum: number, product) => sum + product.price, 0)
-    }
-
-    public setDelivery(delivery: HomeDelivery | ShopDelivery): void {
-        this.delivery = delivery;
-    }
-
-    public checkout() {
-        return (this.delivery
-            && this.productList.length > 0) ? "Ok" : "";
-    }
-}
-
-const cart = new Cart();
-cart.addProduct({
-    name: "1",
-    price: 1,
-    id: 1
-})
-
-cart.addProduct({
-    name: "2",
-    price: 2,
-    id: 2
-})
-
-cart.addProduct({
-    name: "3",
-    price: 3,
-    id: 3
-})
-
-cart.deleteProduct(1);
-cart.calculateCostItems();
-
-cart.setDelivery(new HomeDelivery("sss", new Date("2025/05/03")));
-console.log(cart.checkout());
-
+const r = new RealLogger();
+r.logWithDate(new Date(), "сообщение века");
