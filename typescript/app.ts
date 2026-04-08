171f425a2f3ac300bf7f89f35f3666bfd5617a18
The Great White Rabbit <gribomen@mail.ru>
2026-04-08 23:28:07 +0300

    Сделал тяжкое H\W

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,20 +1,97 @@
-abstract class Logger {
-    abstract log(message: string): void;
-    printDate(date: Date) {
-        this.log(date.toString())
+type ElementMap = { key: any, value: any };
+
+class Bucket {
+    private mas: ElementMap[] = [];
+    constructor(element: ElementMap) {
+        this.add(element);
+    }
+
+    add(element: ElementMap) {
+        this.mas.push(element)
+    };
+
+    delete(element: any): boolean {
+        if (this.mas.length > 0) {
+            this.mas = this.mas.filter(item => item.key !== element);
+            return true;
+        }
+
+        return false;
+    };
+
+    get(key: any): any {
+        for (const item of this.mas) {
+            if (key === item.key) {
+                return item;
+            }
+        }
+
     }
 }
 
-class RealLogger extends Logger {
-    log(message: string): void {
-        console.log(message);
+class Map {
+    private buckets: Bucket[] = [];
+    typeKey: any;
+    constructor(iterable?: ElementMap[]) {
+        if (!iterable) return;
+        iterable.forEach(element => {
+            this.set(element.key, element.value);
+        });
     }
 
-    logWithDate(date: Date, message: string): void {
-        this.printDate(date);
-        this.log(message);
+    /**
+     * Функция для определения баккета
+     * @param element - ключ элемента
+     * @returns number захешированный индекс bucket
+     */
+    private hash(element: any): number {
+        let str = element + "";
+        let hash = 0;
+        for (let i = 0; i < str.length; i++) {
+            hash += str.charCodeAt(i);
+        }
+        return hash % this.buckets?.length ? this.buckets?.length : 1;
+    }
+
+    set(key: any, value: any): ElementMap {
+        const hash_id = this.hash(key);
+        if (this.buckets[hash_id]) {
+            this.buckets[hash_id].add({ key, value })
+        } else {
+            this.buckets[hash_id] = new Bucket({ key, value });
+        }
+        return { key, value };
+    }
+
+    delete(key: any): boolean {
+        if (this.buckets.length > 0) {
+            return this.buckets[this.hash(key)]?.delete(key) ? true : false;
+        }
+        return false;
+    }
+
+    get(key: any): any {
+        if (this.buckets.length > 0) {
+            return this.buckets[this.hash(key)]?.get(key);
+        }
+        return undefined
+    }
+
+    clear() {
+        this.buckets = [];
     }
 }
 
-const r = new RealLogger();
-r.logWithDate(new Date(), "сообщение века");
+const map = new Map();
+const obj = { num: 1 };
+map.set("2", 2);
+map.set(2, 3);
+map.set(obj, 3);
+map.delete(2);
+console.log(map);
+console.log(map.delete(2));
+console.log(map.get(obj));
+map.clear();
+
+//const map2 = new Map([[obj, 1]]);
+//console.log(map2.get(obj));
