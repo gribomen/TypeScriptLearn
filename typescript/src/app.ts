532443d93e8d1fc8718e9324c59d8ca10fc1e25e
The Great White Rabbit <gribomen@mail.ru>
2026-04-09 23:22:17 +0300

    Посмотрел как используются типы в классах

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -12,3 +12,23 @@ function getSplitHalf<T>(data: Array<T>): Array<T> {
 }
 
 getSplitHalf<number>([1, 3, 4]);
+
+const splite: <T>(data: Array<T>) => Array<T> = getSplitHalf;
+
+interface ILogLine<T> {
+    timeStamp: Date,
+    data: T
+}
+
+type LogLineType<T> = {
+    timeStamp: Date,
+    data: T
+}
+
+const logLine: ILogLine<{ a: number }> = {
+    timeStamp: new Date(),
+    data: {
+        a: 1
+    }
+}
+
